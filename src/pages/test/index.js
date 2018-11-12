476eb50ae67d9b59b5ce953e398848e45e54f483
zhangdianlei <dianlei.zhang@qq.com>
2018-11-12 16:09:02 +0800

    init

--- src/pages/test/index.js
+++ src/pages/test/index.js
@@ -1,9 +1,3 @@
-/*
- * @Author: Jan-superman 
- * @Date: 2018-09-27 20:38:37 
- * @Last Modified by: Jan-superman
- * @Last Modified time: 2018-10-24 16:11:55
- */
 
 import React, { PureComponent } from 'react';
 import { Button, Toast } from 'antd-mobile';
